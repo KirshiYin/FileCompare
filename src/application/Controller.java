99f9f90386bd246fc4be685321020e344af36b2c
kirshiyin <my_last_thug@abv.bg>
2016-09-04 22:30:51 +0200

    browse functionality works

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,5 +1,48 @@
 package application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 public class Controller {
+	public Button Browse;
+	public TextArea textArea1;
+	public TextArea textArea2;
+	public Stage primaryStage;
+	public File selectedFile;
 	
+	public void displayFile(){
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		 selectedFile = fileChooser.showOpenDialog(primaryStage);
+		 TextArea textAreaToFill = null;
+		 try {
+			Scanner s = new Scanner(new File(selectedFile.toString()));
+			if(textArea1.getText().trim().equals("")){
+				textAreaToFill = textArea1;
+			}else {
+				textAreaToFill = textArea2;
+			}
+			while(s.hasNext()){
+				if(s.hasNext()){
+					textAreaToFill.appendText(s.next() + " ");
+				}
+					//Platform.runLater(() -> textArea.appendText(s.next()));
+					// System.out.println(s.next());
+				}
+			s.close();
+			//}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (NoSuchElementException ne){
+			System.out.println(ne.getMessage());
+		}
+		
+	}
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,21 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			HBox root = (HBox)FXMLLoader.load(getClass().getResource("Layout.fxml"));
-			Scene scene = new Scene(root,400,400);
+			primaryStage.setTitle("FileCompare");
+			SplitPane root = (SplitPane)FXMLLoader.load(getClass().getResource("Layout.fxml"));
+			Scene scene = new Scene(root,600,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -20,6 +23,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+
 	
 	public static void main(String[] args) {
 		launch(args);
